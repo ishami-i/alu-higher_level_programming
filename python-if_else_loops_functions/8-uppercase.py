1910778641c27396077d2d23832a95ff61e49ec4
ishami-i <i.irene@alustudent.com>
2025-06-25 11:15:49 -0700

    prints a string in uppercase followed by a new line.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    """Check if a character is a lowercase letter."""
-    if not isinstance(c, str) or len(c) != 1:
-        return False
     char = ord(c)
     if char >= 97 and char <= 122:
         return True

a3dd29b1c6e6312d6fa09b9cd35ed8a356e8693a
ishami-i <i.irene@alustudent.com>
2025-07-04 03:53:24 -0700

    defines a square [size(self)]to retrieve it

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,12 +5,12 @@
 class Square:
     """a classs that defines and initializes an instance
 
-
     Attributes:
         __size: size of the instance
     methods:
         __init__: initialises the instance of the class
         area: computes the area of the instance
+        my_print: prints the square with '#'
     """
     @property
     def size(self):
@@ -20,7 +20,6 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
@@ -31,3 +30,10 @@ class Square:
 
     def area(self):
         return self.size * self.size
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)

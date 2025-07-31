c2fbe064bfdb56bfdec483e92df11fb0b32af0fc
ishami-i <i.irene@alustudent.com>
2025-07-31 14:56:36 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""A script that usese the request library to make a request"""
+"""Fetches https://alu-intranet.hbtn.io/status and displays the response info"""
 
 import requests
+
 if __name__ == "__main__":
-    with requests.get("https://alx-intranet.hbtn.io/status") as response:
-        print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
+    response = requests.get("https://alu-intranet.hbtn.io/status")
+    content = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))

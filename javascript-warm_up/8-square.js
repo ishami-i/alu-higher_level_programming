af7c1b0e483bee3c3651202d367cfb94bb7ae034
ishami-i <i.irene@alustudent.com>
2025-09-29 03:52:00 -0700

    script that prints a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+let number = Math.floor(argv[2]);
+let x = '';
+if (number) {
+  for (let i = number; i > 0; i--) {
+    x += 'X';
+  }
+  while (number > 0) {
+    console.log(x);
+    number--;
+  }
+} else {
+  console.log('Missing size');
+}
